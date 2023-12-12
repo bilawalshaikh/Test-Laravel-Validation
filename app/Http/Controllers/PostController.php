9a7c264184683c0da0f5cc7d5cbcd0436a177050
bilawal shaikh <46257281+bilawalshaikh@users.noreply.github.com>
2023-12-12 13:44:58 +0500

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use App\Models\Post;
 use Illuminate\Http\Request;
+
 class PostController extends Controller
 {
     public function store(Request $request)
@@ -9,13 +12,12 @@ class PostController extends Controller
         $request->validate(
             // ... TASK: write validation here so that "title" field
             //           would be required and unique in the "posts" DB table
-            [
-            'title' => 'required|unique:posts'
-            ]
+            ['title' => 'required|unique:posts',]
         );
 
         // Saving the post
         Post::create(['title' => $request->title]);
+
         return 'Success';
     }
 }
