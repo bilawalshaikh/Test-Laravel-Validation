29178d5faae8c18609148232fea92417f8cad83b
bilawal shaikh <46257281+bilawalshaikh@users.noreply.github.com>
2023-12-12 11:52:15 +0500

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,8 +11,11 @@ class UserController extends Controller
     {
         // TASK: change this line to not allow is_admin field to be updated
         // Update only the fields that are validated in UpdateUserRequest
-        $user->update($request->all());
-
+//         $user->update($request->except('is_admin'));
+           $validatedData = $request->validated();
+           unset($validatedData['is_admin']); // Remove 'is_admin' from the validated data
+           $user->update($validatedData);
+        
         return 'Success';
     }
 }
